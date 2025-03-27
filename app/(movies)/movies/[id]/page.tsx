f1db3c5098adf799d888410c067c513b8eb0cc9f
namkijoo <rlwn5904@naver.com>
2025-03-27 23:33:40 +0900

    스타일링

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,10 +1,19 @@
 import { Suspense } from "react";
-import MovieInfo from "../../../../components/movie-info";
+import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-export default async function MovieDetail({ params }) {
-  const { id } = params;
+interface IParameter {
+  params: { id: string };
+}
 
+export async function generateMetadata({ params: { id } }: IParameter) {
+  const movie = await getMovie(id);
+  return {
+    title: movie.title,
+  };
+}
+
+export default async function MovieDetail({ params: { id } }: IParameter) {
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info</h1>}>
