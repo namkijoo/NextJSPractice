ec61081732d47666ea1333c3233f9ee7b0e74aa6
namkijoo <rlwn5904@naver.com>
2025-03-27 23:41:17 +0900

    타입 수정

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -2,18 +2,14 @@ import { Suspense } from "react";
 import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-interface IParameter {
+// ⛔ 타입 선언 X – 직접 구조분해해서 타입추론 유도
+export default async function MovieDetail({
+  params,
+}: {
   params: { id: string };
-}
+}) {
+  const { id } = params;
 
-export async function generateMetadata({ params: { id } }: IParameter) {
-  const movie = await getMovie(id);
-  return {
-    title: movie.title,
-  };
-}
-
-export default async function MovieDetail({ params: { id } }: IParameter) {
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info</h1>}>
@@ -24,4 +20,12 @@ export default async function MovieDetail({ params: { id } }: IParameter) {
       </Suspense>
     </div>
   );
+}
+
+// ✅ 타입 충돌 피하기 위한 정석적인 우회 방법
+export async function generateMetadata({ params }: { params: { id: string } }) {
+  const movie = await getMovie(params.id);
+  return {
+    title: movie.title,
+  };
 }
